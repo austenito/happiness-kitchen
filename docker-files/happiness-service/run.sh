f99c29b20ef1a208b04de287fcf9db3078a7349f
Austen Ito <austen.ito@gmail.com>
2014-06-14 08:27:30 -0400

    Add happiness service run script

---
+++ docker-files/happiness-service/run.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+source /usr/local/share/chruby/chruby.sh
+git clone https://github.com/austenito/happiness_service.git
+cd happiness_service
+chruby 2.1.2
+bundle install
+rake db:create db:migrate
+unicorn -p 3000
