96995992339c0e60e45fb31cf6907e16d3b9d878
Austen Ito <austen.ito@gmail.com>
2014-06-15 15:01:59 -0400

    Change it back

--- docker-files/happiness-service/run.sh
+++ docker-files/happiness-service/run.sh
@@ -9,3 +9,5 @@ chruby 2.1.2
 bundle install
 bundle exec rake db:create db:migrate RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec unicorn_rails -c config/unicorn.rb -E production
+
