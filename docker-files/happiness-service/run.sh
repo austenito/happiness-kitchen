39f64ab7770972127daaf85bfd02b716568487f2
Austen Ito <austen.ito@gmail.com>
2014-06-15 13:24:45 -0400

    Fix run script to run unicorn as a daemon

--- docker-files/happiness-service/run.sh
+++ docker-files/happiness-service/run.sh
@@ -3,8 +3,9 @@
 source /usr/local/share/chruby/chruby.sh
 git clone https://github.com/austenito/happiness_service.git
 cd happiness_service
+mkdir pids
 chruby 2.1.2
 bundle install
 bundle exec rake db:create db:migrate RAILS_ENV=production
-bundle exec rake assets:precompile
-bundle exec unicorn_rails -c config/unicorn.rb -E production
+bundle exec rake assets:precompile RAILS_ENV=production
+bundle exec unicorn_rails -c config/unicorn.rb -E production -D
