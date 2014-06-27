cc08247c5ba51fc5a7f6a1599d657af60ae39125
Austen Ito <austen.ito@gmail.com>
2014-06-27 08:21:44 -0400

    Add console container

---
+++ docker-files/happiness-service-console/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /usr/local/share/chruby/chruby.sh
+git clone https://github.com/austenito/happiness_service.git
+cd happiness_service
+mkdir pids
+mkdir log
+chruby 2.1.2
+bundle install
+bundle exec rake db:create db:migrate RAILS_ENV=production
+/bin/bash
