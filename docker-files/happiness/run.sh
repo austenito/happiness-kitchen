0293d05f5f13ee3e41e4e2ab705e3c7e42cb451d
Austen Ito <austen.ito@gmail.com>
2014-06-29 12:26:11 -0400

    Add local gem installs

--- docker-files/happiness/run.sh
+++ docker-files/happiness/run.sh
@@ -6,7 +6,7 @@ cd happiness
 mkdir pids
 mkdir log
 chruby 2.1.2
-bundle install --without development test
+bundle install --local --without development test
 bundle exec rake db:create db:migrate RAILS_ENV=production
 bundle exec rake assets:precompile RAILS_ENV=production
 bundle exec unicorn_rails -c config/unicorn.rb -E production -p 3001
