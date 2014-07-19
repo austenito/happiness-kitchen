3d79a8ace0866ad0107e77e8af68d2231bcf7e89
Austen Ito <austen.ito@gmail.com>
2014-07-19 13:22:12 -0400

    Fix regex in nginx.conf

--- docker-files/nginx/run.sh
+++ docker-files/nginx/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/http:\/\/.*/${HAPPINESS_PORT_3001_TCP_ADDR}:${HAPPINESS_PORT_3001_TCP_PORT};/" /config/nginx/nginx.conf
+sed -i "s/server .*/server ${HAPPINESS_PORT_3001_TCP_ADDR}:${HAPPINESS_PORT_3001_TCP_PORT};/" /config/nginx/nginx.conf
 /usr/sbin/nginx -c /config/nginx/nginx.conf
