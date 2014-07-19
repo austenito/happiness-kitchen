5ac03e10dbdf3ba20d4a471304732044bb158754
Austen Ito <austen.ito@gmail.com>
2014-07-19 09:34:46 -0400

    Move nginx config to data volume

--- docker-files/nginx/run.sh
+++ docker-files/nginx/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i "s/@happiness_server/${HAPPINESS_PORT_3001_TCP_ADDR}:${HAPPINESS_PORT_3001_TCP_PORT}/" /etc/nginx/nginx.conf
-/usr/sbin/nginx -c /etc/nginx/nginx.conf
+sed -i "s/@happiness_server/${HAPPINESS_PORT_3001_TCP_ADDR}:${HAPPINESS_PORT_3001_TCP_PORT}/" /config/nginx/nginx.conf
+/usr/sbin/nginx -c /config/nginx/nginx.conf
